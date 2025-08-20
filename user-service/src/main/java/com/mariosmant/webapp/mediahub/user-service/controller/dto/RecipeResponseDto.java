ed34d1bf569ab68249460fa08568a6cea4b0cb17
mariosmant <mariosmant@gmail.com>
2025-08-20 15:16:30 +0300

    Regenerated maven wrapper for latest Maven and updated folder hierarchy of microservices and parent/common dependencies.

---
+++ user-service/src/main/java/com/mariosmant/webapp/mediahub/user-service/controller/dto/RecipeResponseDto.java
@@ -0,0 +1,11 @@
+package com.mariosmant.webapp.mediahub.core.controller.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class RecipeResponseDto {
+    private String name;
+    private String description;
+}
